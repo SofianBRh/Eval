a9acd3ad5089184b0de98849e5c9bc70de17e82b
SofianBRh <95184450+SofianBRh@users.noreply.github.com>
2022-08-10 13:27:38 +0200

    Add files via upload

---
+++ indexx.js
@@ -0,0 +1,203 @@
+//fichier JSON  dedicace a vicent 
+
+
+
+var jsonData = [
+    {
+      "question": "Qui est le premier hokage ?",
+      "choix1": "Hashirama",
+      "choix2" :"Kakashi",
+      "choix3": "Naruto",
+      "choix4": "Minato",
+      "answer":  "Hashirama" ,
+      "anecdote" : "bg du 30"
+    },
+    {
+       "question": "Qui a detruit Konoha ?",
+       "choix1": "Pain",
+       "choix2":"Nagato",
+       "choix3": "Konan",
+       "choix4": "Orochimaru",
+       "answer": "Pain",
+       "anecdote" : "bg du 30"
+     },
+     {
+       "question": "Comment Naruto appelle t'il jiraya ?",
+       "choix1": "sensei",
+       "choix2":"l'ermite pas net ",
+       "choix3": "Maitre",
+       "choix4": "Bg du 30",
+       "answer": "l'ermite pas net ",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Quel est le nom de Minato ?",
+       "choix1": "Uzumaki",
+       "choix2":"Uchiha",
+       "choix3": "Namikaze",
+       "choix4": "Kujo",
+       "answer": "Namikaze",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Qui a tué le clan uchiha ?",
+       "choix1": "Naruto",
+       "choix2":"Hinata",
+       "choix3": "Itachi",
+       "choix4": "Sasuke",
+       "answer": "Itachi",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Qui est le frère de Sasuke ? ",
+       "choix1": "NAruto",
+       "choix2":"Minato",
+       "choix3": "Sarutobi",
+       "choix4": "Itachi",
+       "answer": "Itachi",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Qui est la première personne à avoir apprécié Naruto",
+       "choix1": "Hinata",
+       "choix2":"Iruka",
+       "choix3": "Sasuke",
+       "choix4": "Sakura",
+       "answer":"Iruka",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Kakashi est surnommé :",
+       "choix1": "Le ninja copieur",
+       "choix2":"Le ninja au sharingan",
+       "choix3": "Le ninja au bandeu",
+       "choix4": "KA KA SHIIIII",
+       "answer":  "Le ninja copieur",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Quel grand méchant fait partie de la famille de Naruto ?",
+       "choix1": "PAin",
+       "choix2":"Nagato",
+       "choix3": "Karin",
+       "choix4": "Kushina",
+       "answer": "Nagato",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Quelle fille aime Naruto ?",
+       "choix1": "Sakura",
+       "choix2":"Ino",
+       "choix3": "Hinata",
+       "choix4": "Tenten",
+       "answer": "Hinata",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Dans le film Naruto road to ninja quelle particularité y a-t-il ?",
+       "choix1": "Naruto n'existe pas",
+       "choix2":"Naruto est le Bg du village",
+       "choix3": "Naruto est hokage",
+       "choix4": "Les parent de naruto sont en vie",
+       "answer": "Les parent de naruto sont en vie",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Quel bras Naruto a-t-il perdu ?",
+       "choix1": "Le gauche",
+       "choix2":"Le droit",
+       "choix3": "Les deux",
+       "choix4": "Aucun",
+       "answer": "Les parent de naruto sont en vie",
+       "anecdote" : "bg du 30"
+     },
+   
+     {
+       "question": "Contre qui Naruto a-t-il perdu son bras ?",
+       "choix1": "Itachi",
+       "choix2":"Kakshi",
+       "choix3": "Sasuke",
+       "choix4": "Iwari",
+       "answer": "Sasuke",
+       "anecdote" : "bg du 30"
+     },
+     {
+       "question": "Quel était le surnom de Kushina ?",
+       "choix1": "La moche",
+       "choix2":"L'etrangere",
+       "choix3": "Le piment rouge sang",
+       "choix4": "La fille aux cheveux rouge ",
+       "answer": "Le piment rouge sang",
+       "anecdote" : "bg du 30"
+     },
+    {
+       "question": "Quand Kushina est-elle apparue dans l'esprit de Naruto ?",
+       "choix1": "Quand il dormait",
+       "choix2":"quand il pensait ",
+       "choix3": "quand il a combattu Kiyubi",
+       "choix4": "Quand il mangé",
+       "answer": "quand il a combattu Kiyubi",
+       "anecdote" : "bg du 30"
+     },{
+       "question": "Naruto est la réincarnation de :",
+       "choix1": "Ashura",
+       "choix2":"indra",
+       "choix3": "Minato",
+       "choix4": "Tobirama",
+       "answer": "indra",
+       "anecdote" : "bg du 30"
+     },{
+       "question": "Quel est le rang de Naruto parmi les hokages ?",
+       "choix1": "cinquieme",
+       "choix2":"sixieme",
+       "choix3": "septieme",
+       "choix4": "huitieme",
+       "answer": "septieme",
+       "anecdote" : "bg du 30"
+     },{
+       "question": "Combien de frères avait Hashirama ?",
+       "choix1": "un",
+       "choix2":"deux",
+       "choix3": "trois",
+       "choix4": "quatre",
+       "answer": "deux",
+       "anecdote" : "bg du 30"
+     },
+     {
+       "question": "Quel âge a Naruto quand il rencontre ses parents ?",
+       "choix1": "treize ans",
+       "choix2":"quatorze ans",
+       "choix3": "quinze ans",
+       "choix4": "seize ans",
+       "answer": "seize ans",
+       "anecdote" : "bg du 30"
+     },
+     {
+       "question": " Qui est Akamaru",
+       "choix1": "un chat",
+       "choix2":"le pote a pakun",
+       "choix3": "Un chien ",
+       "choix4": "Le chien de kiba",
+       "answer": "Le chien de kiba",
+       "anecdote" : "bg du 30"
+     },
+     
+     {
+       "question": "Qui est mort dans le premier épisode de Naruto ?",
+       "choix1": "le premere hokage",
+       "choix2":"le second hogake",
+       "choix3": "le troisieme hokage",
+       "choix4": "Le quatrieme hokage",
+       "answer":  "Le quatrieme hokage",
+       "anecdote" : "bg du 30"
+     }
+    ]
